efcf07725873bf136f6e9eb4cb389635022ab200
Ivan <ivan.poganiuko@gmail.com>
2020-09-03 16:11:50 +0300

    calculator

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 'use strict';
 
-let user = {
-  name: 'ivan',
-  age: 12,
-  toString(){
-    return this.name;
-  },
-  valueOf(){
-    return this.age; 
-  },
-  [Symbol.toPrimitive](hint){
-    alert(hint);
-    return hint == "string" ? this.toString() : this.valueOf();
-  }
-};
+function Calculator(){
+  this.value1 = 0;
+  this.value2 = 0;
 
-alert(12 + user);
+  this.read = function(){
+    this.value1 = +prompt('Введите значение 1');
+    this.value2 = +prompt('Введите значение 2');
+  }
+
+  this.sum = function(){
+    return this.value1 + this.value2;
+  }
+
+  this.mul = function(){
+    return this.value1 * this.value2;
+  }
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
