adddf31a466a33027d6697020f9bf46ec3db6381
Ivan <ivan.poganiuko@gmail.com>
2020-09-04 22:02:01 +0300

    last-args

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 'use strict';
 
-let options = {
-   header: "CustomHeader",
-   isTop: true
-};
+let arr = [2,6,1,7,4,5,7,8,2,10,2,1,4];
 
-alert( JSON.stringify(options, undefined, 2) );
+alert( max(...arr) );
+
+function max(...args){
+   let max = args[0];
+   for (let arg of args)
+      if (arg > max)
+         max = arg;
+
+   return max;
+}
