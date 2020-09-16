3b86788dfc7644255c6b413ecac11d5e22a4fbb2
Ivan <ivan.poganiuko@gmail.com>
2020-09-16 12:44:37 +0300

    get-set

--- main.js
+++ main.js
@@ -1,51 +1,25 @@
 'use strict';
 
-let obj1 = new Obj("Ivan");
-let obj2 = new Obj("Oleg");
-
-let f1000 = throttle(obj1.logName.bind(obj1), 1000);
-let f2000 = throttle(obj2.logName.bind(obj2), 1000);
-
-f1000(1); // показывает 1
-f2000(1); // (ограничение, 1000 мс ещё нет)
-f1000(2);
-f2000(2);
-f2000(3);
-
-
-function throttle(func, ms) {
-  let lastArgs;
-  let isNeedUpd = false;
-  let isReadyForUpd = true;
-
-  const update = () => {
-    if (isReadyForUpd && isNeedUpd) {
-      isReadyForUpd = false;
-      isNeedUpd = false;
-      func(...lastArgs);
-      setTimeout(() => {
-        isReadyForUpd = true;
-        update();
-      }, ms);
-    }
-  }; 
-
-  return function(...args) {
-    isNeedUpd = true;
-    lastArgs = args;
-    update();
+let obj = {
+  _name: "Ivan",
+  get name(){
+    return this._name;
+  },
+  set name(value) {
+    if (value !== "Oleg")
+      this._name = value;
   }
 }
 
-function Obj(name){
-  return {
-    name,
-    logName(c) {
-      alert( `${c}: ${this.name}`);
-    }
+Object.defineProperties(obj, {
+  age: {
+    value: 19,
+    enumerable: true,
+    writable: false,
+    configurable: false 
   }
-}
+});
+let descr = Object.getOwnPropertyDescriptor(obj, "age");
 
-function f(a) {
-  console.log(a)
-}
+alert( obj.age );
+
